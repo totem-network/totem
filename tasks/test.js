d295c7ed9d416b9758b63933e2943976704b54c0
Daniel Wagner <daniel.wagner@totem.network>
2019-05-13 12:28:55 +0200

    Make testing work

--- tasks/test.js
+++ tasks/test.js
@@ -2,6 +2,55 @@ const gulp = require('gulp');
 const mocha = require('gulp-mocha');
 const webpack = require('webpack-stream');
 const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+const webpackConfig = {
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|tests)/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.tsx?$/,
+                exclude: /(node_modules|tests)/,
+                use: ['babel-loader', 'ts-loader']
+            },
+            {
+                test: /\.(graphql|gql)$/,
+                exclude: /node_modules/,
+                loader: 'graphql-tag/loader'
+            },
+            {
+                test: /\.test.tsx?$/,
+                exclude: /(node_modules)/,
+                use: ['babel-loader', 'ts-loader']
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            '3box': path.resolve('./node_modules/3box/dist/3box.min.js'),
+        },
+        aliasFields: ['browser'],
+        extensions: ['.mjs', '.ts', '.tsx', '.js', '.json'],
+        mainFields: ['browser', 'module', 'main'],
+        modules: [
+            path.resolve('./app/src'),
+            path.resolve('./app/assets'),
+            path.resolve('./build/contracts'),
+            path.resolve('./node_modules')
+        ],
+        plugins: [
+            new TsconfigPathsPlugin({})
+        ]
+    },
+    node: {
+        fs: 'empty',
+        child_process: 'empty'
+    }
+};
 
 gulp.task('test', function() {
     
@@ -9,68 +58,36 @@ gulp.task('test', function() {
 
 gulp.task('test:integration', function() {
     return gulp.src('tests/app/integration/**/*.test.ts*')
-        .pipe(webpack({
-            module: {
-                exprContextCritical: false,
-                rules: [
-                    {
-                        test: /\.tsx?$/,
-                        exclude: /(node_modules|tests)/,
-                        use: ['babel-loader', 'ts-loader']
-                    },
-                    {
-                        test: /\.test.tsx?$/,
-                        exclude: /(node_modules)/,
-                        use: ['babel-loader', 'ts-loader']
-                    }
-                ]
-            },
-            resolve: {
-                extensions: ['.ts', '.tsx', '.js'],
-                modules: [
-                    path.resolve('./app/src'),
-                    path.resolve('./app/assets'),
-                    path.resolve('./node_modules')
-                ]
-            },
-            node: {
-                fs: 'empty',
-            }
-        }))
+        .pipe(webpack(webpackConfig))
         .pipe(gulp.dest('tests/build'))
-        .pipe(mocha());
+        .pipe(mocha({
+            require: [
+                'jsdom-global/register'
+            ]
+        }))
+        .once('error', err => {
+            console.error(err);
+            process.exit(1);
+        })
+        .once('end', () => {
+            process.exit();
+        });
 });
 
 gulp.task('test:unit', function() {
     return gulp.src('tests/app/unit/**/*.test.ts*')
-        .pipe(webpack({
-            module: {
-                exprContextCritical: false,
-                rules: [
-                    {
-                        test: /\.tsx?$/,
-                        exclude: /(node_modules|tests)/,
-                        use: ['babel-loader', 'ts-loader']
-                    },
-                    {
-                        test: /\.test.tsx?$/,
-                        exclude: /(node_modules)/,
-                        use: ['babel-loader', 'ts-loader']
-                    }
-                ]
-            },
-            resolve: {
-                extensions: ['.ts', '.tsx', '.js'],
-                modules: [
-                    path.resolve('./app/src'),
-                    path.resolve('./app/assets'),
-                    path.resolve('./node_modules')
-                ]
-            },
-            node: {
-                fs: 'empty',
-            }
-        }))
+        .pipe(webpack(webpackConfig))
         .pipe(gulp.dest('tests/build'))
-        .pipe(mocha());
+        .pipe(mocha({
+            require: [
+                'jsdom-global/register'
+            ]
+        }))
+        .once('error', err => {
+            console.error(err);
+            process.exit(1);
+        })
+        .once('end', () => {
+            process.exit();
+        });
 });
