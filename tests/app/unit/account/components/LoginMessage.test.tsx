18e4c67e8d1cc9f41dad1e29bd6401132fb559b5
Daniel Wagner <daniel.wagner@totem.network>
2019-05-13 16:54:53 +0200

    Test login message

---
+++ tests/app/unit/account/components/LoginMessage.test.tsx
@@ -0,0 +1,30 @@
+import '@babel/polyfill';
+import LoginMessage from 'account/components/LoginMessage';
+import {
+    expect,
+    use as chaiUse,
+} from 'chai';
+import chaiEnzyme from 'chai-enzyme';
+import {
+    configure as configureEnzyme,
+    render,
+} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import 'mocha';
+import React from 'react';
+
+configureEnzyme({
+    adapter: new Adapter(),
+});
+
+chaiUse(chaiEnzyme());
+
+describe('Account components', () => {
+    describe('<LoginMessage />', () => {
+        it('should render "Hello"', () => {
+            const wrapper = render(<LoginMessage />);
+
+            expect(wrapper.text()).to.contain('Hello');
+        });
+    });
+});
