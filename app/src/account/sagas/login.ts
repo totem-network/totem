de962e186c0fb128ccf57d350632f3f93a6c7960
Daniel Wagner <daniel.wagner@totem.network>
2020-03-31 09:46:18 +0200

    Open 3box on login

--- app/src/account/sagas/login.ts
+++ app/src/account/sagas/login.ts
@@ -1,12 +1,14 @@
 import { utils, Wallet } from 'ethers';
 import { call, put, takeLatest } from 'redux-saga/effects';
+import { getCurrentNetworkSigner } from 'utils/blockchain';
 import {
     ILoginMetaMaskAction,
     ILoginPrivateKeyAction,
     LOGIN_METAMASK,
     LOGIN_PRIVATE_KEY,
     loginSuccess,
-} from './../actions/login';
+} from '../actions/login';
+import boxes from '../profile/boxes';
 
 export const createWallet = (privateKey: string) => {
     return new Wallet(privateKey);
@@ -26,6 +28,13 @@ export const getProvidedAccounts = () => {
     return web3.eth.accounts;
 };
 
+export function* openBox(address: string) {
+    const currentSigner = yield call(getCurrentNetworkSigner);
+    const wrappedSigner = yield call(boxes.wrapEthersSigner, currentSigner);
+
+    return yield call([boxes, boxes.openBox], address, wrappedSigner);
+}
+
 export function* loginWithPrivateKey(action: ILoginPrivateKeyAction) {
     const privateKey = yield call(utils.hexlify, action.payload.privateKey);
 
@@ -33,9 +42,9 @@ export function* loginWithPrivateKey(action: ILoginPrivateKeyAction) {
     // import TestWorker from 'worker-loader!test.worker';
     const account = yield call(createWallet, privateKey);
 
-    // TODO: openBox on login before loginSuccess
-
     if (account) {
+        yield call(openBox, account.address);
+
         yield put(loginSuccess(account.address));
     } else {
         // TODO: put login error
@@ -45,9 +54,9 @@ export function* loginWithPrivateKey(action: ILoginPrivateKeyAction) {
 export function* loginWithMetaMask(action: ILoginMetaMaskAction) {
     const accounts = yield call(getProvidedAccounts);
 
-    // TODO: openBox on login before loginSuccess
-
     if (accounts && accounts[0]) {
+        yield call(openBox, accounts[0]);
+
         yield put(loginSuccess(accounts[0]));
     } else {
         // TODO: put login error
