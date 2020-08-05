3287895e456656a0c2dacdcd753acb504fc14388
Daniel Wagner <daniel.wagner@vinyai.com>
2020-08-05 15:01:29 +0200

    Document code

--- app/src/account/actions/create.ts
+++ app/src/account/actions/create.ts
@@ -2,18 +2,33 @@ import { Action } from 'redux';
 
 // create account
 
+/**
+ * @constant {string} CREATE_ACCOUNT - Identitfy create account action
+ */
 export type CREATE_ACCOUNT = 'account/CREATE_ACCOUNT';
 export const CREATE_ACCOUNT: CREATE_ACCOUNT = 'account/CREATE_ACCOUNT';
 
+/**
+ * Interface for create account action object
+ * @interface
+ */
 export interface ICreateAccountAction extends Action {
     type: CREATE_ACCOUNT;
     payload: ICreateAccountPayload;
 }
 
+/**
+ * Payload of the create account action
+ * @interface
+ */
 export interface ICreateAccountPayload {
     privateKey: string;
 }
 
+/**
+ * Creates the create account action object with given paramaters
+ * @param {string} privateKey - Private key of the account
+ */
 export function createAccount(privateKey: string): ICreateAccountAction {
     return {
         payload: {
@@ -23,9 +38,18 @@ export function createAccount(privateKey: string): ICreateAccountAction {
     };
 }
 
+/**
+ * Extends Action to have an optional payload
+ * @interface
+ * @extends Action
+ */
 interface IOtherAction extends Action {
     payload?: any;
 }
 
+/**
+ * Combines all create account actions for the reducer
+ * @type
+ */
 export type CreateAccountAction = ICreateAccountAction |
         IOtherAction;
--- app/src/account/actions/login.ts
+++ app/src/account/actions/login.ts
@@ -2,18 +2,33 @@ import { Action } from 'redux';
 
 // login with private key
 
+/**
+ * @constant {string} LOGIN_PRIVATE_KEY - Identitfy login private key action
+ */
 export type LOGIN_PRIVATE_KEY = 'account/LOGIN_PRIVATE_KEY';
 export const LOGIN_PRIVATE_KEY: LOGIN_PRIVATE_KEY = 'account/LOGIN_PRIVATE_KEY';
 
+/**
+ * Interface for login private key action object
+ * @interface
+ */
 export interface ILoginPrivateKeyAction extends Action {
     type: LOGIN_PRIVATE_KEY;
     payload: ILoginPrivateKeyPayload;
 }
 
+/**
+ * Payload of the login private key action
+ * @interface
+ */
 export interface ILoginPrivateKeyPayload {
     privateKey: string;
 }
 
+/**
+ * Creates the login private key action object with given paramaters
+ * @param {string} privateKey - Private key of the account
+ */
 export function loginPrivateKey(privateKey: string): ILoginPrivateKeyAction {
     return {
         payload: {
@@ -23,20 +38,34 @@ export function loginPrivateKey(privateKey: string): ILoginPrivateKeyAction {
     };
 }
 
-// login with meta mask
+// login with metamask
 
+/**
+ * @constant {string} LOGIN_METAMASK - Identitfy login metamask action
+ */
 export type LOGIN_METAMASK = 'account/LOGIN_METAMASK';
 export const LOGIN_METAMASK: LOGIN_METAMASK = 'account/LOGIN_METAMASK';
 
+/**
+ * Payload of the login metamask action
+ * @interface
+ */
 export interface ILoginMetaMaskAction extends Action {
     type: LOGIN_METAMASK;
     payload: ILoginMetaMaskPayload;
 }
 
+/**
+ * Payload of the login metamask action
+ * @interface
+ */
 export interface ILoginMetaMaskPayload {
     //
 }
 
+/**
+ * Creates the login metamask action object
+ */
 export function loginMetaMask(): ILoginMetaMaskAction {
     return {
         payload: {},
@@ -46,18 +75,33 @@ export function loginMetaMask(): ILoginMetaMaskAction {
 
 // login success
 
+/**
+ * @constant {string} LOGIN_SUCCESS - Identitfy login success action
+ */
 export type LOGIN_SUCCESS = 'account/LOGIN_SUCCESS';
 export const LOGIN_SUCCESS: LOGIN_SUCCESS = 'account/LOGIN_SUCCESS';
 
+/**
+ * Payload of the login success action
+ * @interface
+ */
 export interface ILoginSuccessAction extends Action {
     type: LOGIN_SUCCESS;
     payload: ILoginSuccessPayload;
 }
 
+/**
+ * Payload of the login success action
+ * @interface
+ */
 export interface ILoginSuccessPayload {
     address: string;
 }
 
+/**
+ * Creates the login success action object with given paramaters
+ * @param {string} address - Address of the account
+ */
 export function loginSuccess(address: string): ILoginSuccessAction {
     return {
         payload: {
@@ -67,10 +111,19 @@ export function loginSuccess(address: string): ILoginSuccessAction {
     };
 }
 
+/**
+ * Extends Action to have an optional payload
+ * @interface
+ * @extends Action
+ */
 interface IOtherAction extends Action {
     payload?: any;
 }
 
+/**
+ * Combines all login actions for the reducer
+ * @type
+ */
 export type LoginAction = ILoginPrivateKeyAction |
         ILoginMetaMaskAction |
         ILoginSuccessAction |
